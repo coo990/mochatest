8094b9e37e95aa3447c67fcd918a9206d5063a45
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-10-03 14:01:51 +0100

    test: function returns hello test

--- test/apptest.js
+++ test/apptest.js
@@ -1,4 +1,8 @@
-const asser = require('chai').assert;
+const assert = require('chai').assert;
 const app = require('../app');
 
-describe('App');
+describe('App', function(){
+  it('app should return hello', function(){
+    assert.equal(app(), 'hello');
+  })
+});
